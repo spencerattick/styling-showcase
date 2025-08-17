f501a8bd061296a78635b6dd4f87a4b0dbeb9496
Spencer Attick <spencerattick@Spencers-MacBook-Pro.local>
2025-08-17 16:40:42 -0700

    add faq content

--- src/app/javascript/faq/page.tsx
+++ src/app/javascript/faq/page.tsx
@@ -1,9 +1,60 @@
-import backgroundImage from '../../../public/background-pattern-desktop.svg'
+import Image from "next/image";
 
 export default function FAQ() {
+  const content = [
+    {
+      question: "What is Frontend Mentor, and how will it help me?",
+      answer:
+        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and Javascript. It's suitable for all levels and ideal for portfolios.",
+    },
+    {
+      question: "Is Frontend Mentor free?",
+      answer:
+        "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+    },
+    {
+      question: "Can I use Frontend Mentor projects in my portfolio?",
+      answer:
+        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+    },
+    {
+      question:
+        "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+      answer:
+        "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+    },
+  ];
   return (
-    <div className=`bg-[${backgroundImage}]`>
+    <div className="relative min-h-screen">
+      <div className="h-[35vh] bg-[url('/background-pattern-desktop.svg')]" />
 
+      <div className="h-[60vh] bg-[#f9f0ff]" />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-2xl p-6">
+        <div className="bg-white rounded-lg shadow-lg p-8 h-[50vh]">
+          <div className="flex gap-4 mb-5">
+            <Image
+              src="/icon-star.svg"
+              height={30}
+              width={30}
+              alt="star icon"
+              className=""
+            />
+            <h1 className="text-4xl font-[1000] text-[#2f1632]">FAQs</h1>
+          </div>
+          <div>
+            {content.map((item, index) => {
+              return (
+                <div key={index}>
+                  <h3 className="text-[#2f1632] font-semibold mb-2">
+                    {item.question}
+                  </h3>
+                  <p className="text-[#b1a6b2]">{item.answer}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-center text-6xl mt-5">Styling Practice</h1>
-      <div className="flex justify-center text-2xl mt-10">
+      <div className="flex flex-col justify-center items-center text-2xl mt-10">
         <Link href="/cards">Cards</Link>
         <Link href="/javascript">Designs with Javascript</Link>
       </div>
