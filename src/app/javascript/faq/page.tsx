7c9a21fffc01f680ea794b71a9205675997598d2
Spencer Attick <spencer.attick@gmail.com>
2025-09-21 07:00:12 -0700

    open or close FAQs on click

--- src/app/javascript/faq/page.tsx
+++ src/app/javascript/faq/page.tsx
@@ -3,37 +3,43 @@
 import Image from "next/image";
 import { useState } from "react";
 
-export default function FAQ() {
-  const [isOpenIndex, setIsOpenIndex] = useState<number | null>(null);
+const content = [
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and Javascript. It's suitable for all levels and ideal for portfolios.",
+  },
+  {
+    question: "Is Frontend Mentor free?",
+    answer:
+      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  },
+  {
+    question: "Can I use Frontend Mentor projects in my portfolio?",
+    answer:
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+  },
+  {
+    question:
+      "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    answer:
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  },
+];
 
-  const handleIconClick = (index: number | null) => {
-    setIsOpenIndex(index);
+export default function FAQ() {
+  const [openStates, setOpenStates] = useState<boolean[]>(Array(content.length).fill(false));
+
+
+  const handleIconClick = (index: number) => {
+    setOpenStates(prev => {
+      const newStates = [...prev];
+      newStates[index] = !newStates[index];
+      return newStates;
+    })
   };
 
-  const content = [
-    {
-      question: "What is Frontend Mentor, and how will it help me?",
-      answer:
-        "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and Javascript. It's suitable for all levels and ideal for portfolios.",
-    },
-    {
-      question: "Is Frontend Mentor free?",
-      answer:
-        "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
-    },
-    {
-      question: "Can I use Frontend Mentor projects in my portfolio?",
-      answer:
-        "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
-    },
-    {
-      question:
-        "How can I get help if I'm stuck on a Frontend Mentor challenge?",
-      answer:
-        "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
-    },
-  ];
-  
+
   return (
     <div className="relative min-h-screen">
       <div className="h-[35vh] bg-[url('/background-pattern-desktop.svg')]" />
@@ -52,7 +58,7 @@ export default function FAQ() {
           </div>
           <div>
             {content.map((item, index) => {
-              const isOpen = isOpenIndex === index;
+              const isOpen = openStates[index];
               return (
                 <div key={index}>
                   <div className="flex items-center gap-2">
