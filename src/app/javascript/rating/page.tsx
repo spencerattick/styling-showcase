b9c3b98abf0e9d12d5a07a5dc803b32c3b4a88e2
Spencer Attick <spencer.attick@gmail.com>
2025-09-22 13:09:30 -0700

    add more styling

--- src/app/javascript/rating/page.tsx
+++ src/app/javascript/rating/page.tsx
@@ -1,21 +1,31 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Rating() {
+  const ratingNums = [1, 2, 3, 4, 5];
+
   return (
-    <div>
-        <div>
-            <h1>How did we do?</h1>
-            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-        </div>
-        <Image src="/icon-star-rating.svg" width={50} height={50} alt='star icon'/>
-        <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
+    <div className="flex items-center justify-center h-[100vh] w-[100vw]">
+      <div className="bg-[#202630] w-[20vw] h-[40vh] rounded-3xl p-10">
+        <Image
+          src="/icon-star-rating.svg"
+          width={30}
+          height={30}
+          alt="star icon"
+          className="bg-#282e38 p-2 rounded-full"
+        />
+
+        <h1 className="font-bold text-2xl mt-5">How did we do?</h1>
+        <p className="text-[#6c727c] text-xs">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <div className="flex justify-around  gap-5">
+          {ratingNums.map((num) => (
+              <button className="bg-[#282e38] rounded-full p-3 h-10 w-10 text-center" key={num}>{num}</button>
+          ))}
         </div>
         <button>Submit</button>
+      </div>
     </div>
-  )
+  );
 }
