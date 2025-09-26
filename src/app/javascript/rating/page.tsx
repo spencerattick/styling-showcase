045a7d45fdd691c9e6d77f4f27c016f818271e53
Spencer Attick <spencer.attick@gmail.com>
2025-09-26 06:29:09 -0700

    finish initial styling

--- src/app/javascript/rating/page.tsx
+++ src/app/javascript/rating/page.tsx
@@ -5,26 +5,26 @@ export default function Rating() {
 
   return (
     <div className="flex items-center justify-center h-[100vh] w-[100vw]">
-      <div className="bg-[#202630] w-[20vw] h-[40vh] rounded-3xl p-10">
+      <div className="bg-[#202630] w-[23vw] h-[40vh] rounded-3xl p-8">
         <Image
           src="/icon-star-rating.svg"
-          width={30}
-          height={30}
+          width={50}
+          height={50}
           alt="star icon"
-          className="bg-#282e38 p-2 rounded-full"
+          className="bg-[#282e38] p-4 rounded-full"
         />
 
-        <h1 className="font-bold text-2xl mt-5">How did we do?</h1>
-        <p className="text-[#6c727c] text-xs">
+        <h1 className="font-bold text-2xl my-5">How did we do?</h1>
+        <p className="text-[#6c727c] text-[15px]">
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <div className="flex justify-around  gap-5">
+        <div className="flex justify-around gap-5">
           {ratingNums.map((num) => (
-              <button className="bg-[#282e38] rounded-full p-3 h-10 w-10 text-center" key={num}>{num}</button>
+              <button className="bg-[#282e38] rounded-full flex items-center justify-center h-10 w-10 text-center text-[#8b939f] mt-5 mb-5" key={num}>{num}</button>
           ))}
         </div>
-        <button>Submit</button>
+        <button className="bg-[#fd7612] text-[#1b212b] w-full mt-5 py-3 text-xs font-semibold tracking-[.15em] rounded-full">SUBMIT</button>
       </div>
     </div>
   );
