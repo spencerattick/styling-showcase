94fe1de5b0018006bfd8b9de69bfb2143997b2cd
Spencer Attick <spencer.attick@gmail.com>
2025-09-28 13:15:56 -0700

    fix inverted colors cards

--- src/app/cards/qr-code/page.tsx
+++ src/app/cards/qr-code/page.tsx
@@ -3,7 +3,7 @@ import qrImage from "../../../../public/image-qr-code.png";
 
 export default function QRCode() {
   return (
-    <div className="flex flex-col items-center justify-center mt-10 h-screen w-full">
+    <div className="flex flex-col items-center justify-center h-screen w-full bg-[#d6e1ef]">
       <div className="flex flex-col space-y-3 items-center justify-center bg-white rounded-[1.25rem] h-1/2 w-80 p-4">
         <div className="rounded-[1.25rem] overflow-hidden">
           <Image src={qrImage} width={280} height={280} alt="qr code" />
--- src/app/cards/social-links/page.tsx
+++ src/app/cards/social-links/page.tsx
@@ -10,7 +10,7 @@ export default function SocialLinks() {
     "Instagram",
   ];
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-black">
       <div className="bg-[#1f1f1f] p-8 rounded-lg flex flex-col items-center">
         <Image
           src={avatarImg}
@@ -19,18 +19,18 @@ export default function SocialLinks() {
           alt="avatar image"
           className="rounded-full mb-5"
         ></Image>
-        <h1 className="font-bold text-2xl">Jessica Randall</h1>
+        <h1 className="font-bold text-2xl text-white">Jessica Randall</h1>
         <p className="text-[#a6c93c] font-semibold text-xs">
           London, United Kingdom
         </p>
-        <p className="mt-8 text-sm">
+        <p className="mt-8 text-sm text-[#afafaf]">
           &quot;Front-end developer and avid reader.&quot;
         </p>
         <div className="flex flex-col w-full mt-4 ">
           {socialLinks.map((link, index) => (
             <div
               key={index}
-              className="m-1.5 p-2 rounded-lg bg-[#333333] text-center font-semibold text-xs hover:bg-[#a6c93c] hover:text-[#1f1f1f] hover:cursor-pointer transition-all duration-200"
+              className="m-1.5 p-2 rounded-lg bg-[#333333] text-center font-semibold text-xs hover:bg-[#a6c93c] hover:text-[#1f1f1f] hover:cursor-pointer transition-all duration-200 text-white"
             >
               {link}
             </div>
