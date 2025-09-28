2bf8f806b16b1fab7290fdc77cb27f2938270335
Spencer Attick <spencer.attick@gmail.com>
2025-09-28 12:48:52 -0700

    add react bit background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+import Iridescence from "@/components/Iridescence";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-center text-6xl mt-5">Styling Practice</h1>
-      <div className="flex flex-col justify-center items-center text-2xl mt-10">
-        <Link href="/cards">Cards</Link>
-        <Link href="/javascript">Designs with Javascript</Link>
+    <>
+      <div className="fixed top-0 left-0 w-screen h-screen -z-10 bg-red-500">
+    
+        <Iridescence
+          color={[1, 1, 1]}
+          mouseReact={false}
+          amplitude={0.1}
+          speed={1.0}
+        />
       </div>
-    </div>
+
+      <div className="relative z-10 min-h-screen w-full flex flex-col items-center justify-center bg-blue-500/30">
+        <h1 className="text-center text-7xl mt-5 text-white font-black">
+          Styling Practice
+        </h1>
+        <h2 className="mt-10 text-white font-semibold text-4xl">Spencer Attick</h2>
+      </div>
+    </>
   );
 }
