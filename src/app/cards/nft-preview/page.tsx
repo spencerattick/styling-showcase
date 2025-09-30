cec8d2401857f4a8de0c0afc8795944230762976
Spencer Attick <spencer.attick@gmail.com>
2025-09-30 08:27:26 -0700

    finish base NFT styles

--- src/app/cards/nft-preview/page.tsx
+++ src/app/cards/nft-preview/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function NFTPreviewCard() {
   return (
     <div className="bg-[#0c192d] w-[100vw] h-[100vh] flex justify-center items-center">
-      <div className="bg-[#15263f] w-[20%] h-[65%] rounded-2xl p-5 flex flex-col items-center">
+      <div className="bg-[#15263f] w-[20%] h-[60%] rounded-2xl p-5 flex flex-col items-center">
         <Image
           src="/image-equilibrium.jpg"
           alt="glass cube balancing on one corner"
@@ -12,17 +12,46 @@ export default function NFTPreviewCard() {
           width={300}
           className="rounded-2xl"
         />
-        <h1 className="text-white text-xl font-semibold">Equilibrium #3429</h1>
-        <p>Our Equilibrium collection promotes balance and calm.</p>
-        <div>
-          <span>0.014 ETH</span>
-          <span>3 days left</span>
-        </div>
-        <div>
-          <p>
-            Creation of
-            <span>Jules Wyvern</span>
+        <div className="flex flex-col gap-4 mt-5 w-full">
+          <h1 className="text-white text-xl font-semibold">
+            Equilibrium #3429
+          </h1>
+          <p className="text-[#6e85a7]">
+            Our Equilibrium collection promotes balance and calm.
           </p>
+          <div className="flex justify-between items-center border-b border-[#2f415b] pb-4">
+            <span className="text-[#1edcdc] font-semibold flex items-center gap-2">
+              <Image
+                src="/icon-ethereum.svg"
+                alt="ethereum icon"
+                height={10}
+                width={10}
+              />
+              0.014 ETH
+            </span>
+            <span className="text-[#6e85a7] flex items-center gap-2">
+              <Image
+                src="/icon-clock.svg"
+                alt="clock icon"
+                width={20}
+                height={20}
+              />
+              3 days left
+            </span>
+          </div>
+          <div>
+            <p className="text-[#6e85a7] flex items-center">
+              <Image
+                src="/image-avatar.png"
+                alt="sample avatar"
+                height={30}
+                width={30}
+                className="border border-white rounded-full inline-block mr-4"
+              />
+              Creation of
+              <span className="text-[#c0cada] ml-1"> Jules Wyvern</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
