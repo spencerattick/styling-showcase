3e03909c5c26819ad9da0b139d4dd890dfebdd16
Spencer Attick <spencer.attick@gmail.com>
2025-10-06 06:47:12 -0700

    add additional styles to cards page

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,3 +1,5 @@
+import GradientText from "@/components/GradientText";
+import ShinyText from "@/components/ShinyText";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -30,25 +32,40 @@ export default function CardsPage() {
     },
   ];
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-200 to-blue-50 pb-20">
-      <h1 className="text-center text-6xl pt-5">Cards</h1>
+    <div className="min-h-screen bg-black pb-20 text-center">
+      {/* <ShinyText
+        text="Cards"
+        disabled={false}
+        speed={13}
+        className="custom-class text-6xl pt-10"
+      /> */}
+      <GradientText
+        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+        animationSpeed={5}
+        showBorder={false}
+        className="custom-class text-6xl pt-10 font-extrabold cursor-default"
+      >
+        Cards
+      </GradientText>
+
+      {/* <h1 className="text-center text-6xl pt-5">Cards</h1> */}
       <div className="flex flex-wrap justify-center p-10 min-h-screen text-2xl mt-10 gap-10">
         {cards.map((card) => (
           <Link
             key={card.name}
             href={card.link}
-            className="flex flex-col items-center gap-2 font-semibold"
+            className="flex flex-col items-center gap-2 font-semibold "
           >
             {card.name}
-            {
+            <div className="shadow-xl shadow-white/20 rounded-lg">
               <Image
                 src={card.previewImage}
                 width={200}
                 height={200}
                 alt={`${card.name} preview card`}
-                className="rounded-xl hover:opacity-50 hover:w-62 object-cover transition-all duration-300 w-60 h-90 shadow-2xl"
-              ></Image>
-            }
+                className="rounded-xl hover:opacity-50 hover:w-62 object-cover transition-all duration-300 w-60 h-90"
+              />
+            </div>
           </Link>
         ))}
       </div>
