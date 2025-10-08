ea837b36a1f03919b16fcbc6f56fe7d97bc459f1
Spencer Attick <spencer.attick@gmail.com>
2025-10-07 22:11:10 -0700

    add most of the styling for order summary

--- src/app/cards/order-summary/page.tsx
+++ src/app/cards/order-summary/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function OrderSummary() {
   return (
     <div className="min-h-screen bg-[#d6e1ff] flex justify-center items-center">
-      <div className="bg-white rounded-xl shadow mx-60">
+      <div className="bg-white rounded-xl shadow-xl mx-60">
         <Image
           src="/illustration-hero.svg"
           width={400}
@@ -11,22 +11,32 @@ export default function OrderSummary() {
           alt="illustration of a person listening to music and dancing"
           className="rounded-t-xl"
         />
-        <div className="flex flex-col items-center text-center p-5">
-          <h1 className="font-black text-2xl pb-3">Order Summary</h1>
+        <div className="flex flex-col gap-4 items-center text-center p-5">
+          <h1 className="font-black text-2xl">Order Summary</h1>
           <p className="text-[#7e849d] text-sm px-10">
             You can now listen to millions of songs, audiobooks, and podcasts on
             any device anywhere you like!
           </p>
-          <div className="bg-[#f8f9fe] flex gap-4 p-3 my-3 rounded-xl">
-            <Image src='/icon-music.svg' width={30} height={40} alt="music note icon"/>
+          <div className="bg-[#f8f9fe] flex gap-4 p-3 my-3 rounded-xl items-center">
+            <Image
+              src="/icon-music.svg"
+              width={35}
+              height={35}
+              alt="music note icon"
+              className="object-contain"
+            />
             <div>
-              <h2>Annual Plan</h2>
-              <p>$59.99/year</p>
+              <h2 className="font-bold">Annual Plan</h2>
+              <p className="text-[#7e849d]">$59.99/year</p>
             </div>
-            <p>Change</p>
+            <p className="ml-9 underline text-sm cursor-pointer text-[#382ae1] font-semibold">
+              Change
+            </p>
           </div>
-          <button>Proceed to Payment</button>
-          <p>Cancel Order</p>
+          <button className="bg-[#382ae1] text-white rounded-xl font-semibold px-16 py-3 shadow-xl text-sm">
+            Proceed to Payment
+          </button>
+          <p className="text-sm text-[#7e849d] font-semibold">Cancel Order</p>
         </div>
       </div>
     </div>
