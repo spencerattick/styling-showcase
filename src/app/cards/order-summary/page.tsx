be4bbffb69544c39d563d7995c42967093da73f3
Spencer Attick <spencer.attick@gmail.com>
2025-10-07 21:43:45 -0700

    add component content

--- src/app/cards/order-summary/page.tsx
+++ src/app/cards/order-summary/page.tsx
@@ -1,5 +1,22 @@
 export default function OrderSummary() {
   return (
-    <div>OrderSummary</div>
-  )
+    <div className="min-h-screen bg-[#d6e1ff] flex justify-center items-center">
+      <div>
+        <h1>Order Summary</h1>
+        <p>
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+        <div>
+          <div>
+            <h2>Annual Plan</h2>
+            <p>$59.99/year</p>
+          </div>
+          <p>Change</p>
+        </div>
+        <button>Proceed to Payment</button>
+        <p>Cancel Order</p>
+      </div>
+    </div>
+  );
 }
