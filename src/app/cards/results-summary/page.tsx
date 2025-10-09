71ca2d485e6a62bc5fe93e982122c38938a34f7e
Spencer Attick <spencer.attick@gmail.com>
2025-10-09 09:54:33 -0700

    style outlines for results summary

--- src/app/cards/results-summary/page.tsx
+++ src/app/cards/results-summary/page.tsx
@@ -1,3 +1,4 @@
+import { div } from "motion/react-client";
 import Image from "next/image";
 
 export default function ResultsSummary() {
@@ -32,28 +33,35 @@ export default function ResultsSummary() {
     },
   ];
   return (
-    <div>
-      <div>
-        <h3>Your Result</h3>
-        <div>
-          <h1>76</h1>
-          <p>of 100</p>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex w-full max-w-sm items-center justify-center rounded-xl border border-green-600">
+        <div className="border border-amber-600 rounded-xl flex flex-col items-center text-center">
+          <h3>Your Result</h3>
+          <div>
+            <h1>76</h1>
+            <p>of 100</p>
+          </div>
+          <h2>Great</h2>
+          <p>
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
         </div>
-        <h2>Great</h2>
-        <p>
-          You scored higher than 65% of the people who have taken these tests.
-        </p>
-      </div>
-      <div>
-        <h2>Summary</h2>
-        <ul>
-          {results.map((result) => (
-            <li key={result.category}>
-              <Image src={result.icon} height={20} width={20} alt={result.alt} />
-            </li>
-          ))}
-        </ul>
-        <button>Continue</button>
+        <div>
+          <h2>Summary</h2>
+          <ul>
+            {results.map((result) => (
+              <li key={result.category}>
+                <Image
+                  src={result.icon}
+                  height={20}
+                  width={20}
+                  alt={result.alt}
+                />
+              </li>
+            ))}
+          </ul>
+          <button>Continue</button>
+        </div>
       </div>
     </div>
   );
