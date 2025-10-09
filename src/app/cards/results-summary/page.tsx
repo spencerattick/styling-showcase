9e8b9cd271ab139b0b1a73426a996fff7a7398be
Spencer Attick <spencer.attick@gmail.com>
2025-10-09 09:36:34 -0700

    add content for results summary

---
+++ src/app/cards/results-summary/page.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+
+export default function ResultsSummary() {
+  const results = [
+    {
+      category: "Reaction",
+      icon: "",
+      alt: "reaction icon",
+      score: 80,
+      color: "bg-red-100 text-red-500",
+    },
+    {
+      category: "Memory",
+      icon: "",
+      alt: "memory icon",
+      score: 92,
+      color: "bg-yellow-100 text-yellow-500",
+    },
+    {
+      category: "Verbal",
+      icon: "",
+      alt: "verbal icon",
+      score: 61,
+      color: "bg-green-100 text-green-500",
+    },
+    {
+      category: "Visual",
+      icon: "",
+      alt: "visual icon",
+      score: 72,
+      color: "bg-blue-100 text-blue-500",
+    },
+  ];
+  return (
+    <div>
+      <div>
+        <h3>Your Result</h3>
+        <div>
+          <h1>76</h1>
+          <p>of 100</p>
+        </div>
+        <h2>Great</h2>
+        <p>
+          You scored higher than 65% of the people who have taken these tests.
+        </p>
+      </div>
+      <div>
+        <h2>Summary</h2>
+        <ul>
+          {results.map((result) => (
+            <li key={result.category}>
+              <Image src={result.icon} height={20} width={20} alt={result.alt} />
+            </li>
+          ))}
+        </ul>
+        <button>Continue</button>
+      </div>
+    </div>
+  );
+}
