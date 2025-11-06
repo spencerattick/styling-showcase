ec8e6152f4a12211c5f1d287d64d2abe25ccecc1
Spencer Attick <spencer.attick@gmail.com>
2025-11-06 15:34:36 -0800

    add content for price grid

---
+++ src/app/cards/price-grid/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function PriceGrid() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Join our community</h1>
+          <h2>30-day, hassle-free money back gaurantee</h2>
+          <p>
+            Gain access to our full library of tutorials along with expert code
+            reviews. Perfect for any developers who are serious about honing
+            their skills.
+          </p>
+        </div>
+        <div>
+          <div>
+            <h2>Monthly Subscription</h2>
+            <h1>
+              $29 <span>per month</span>
+            </h1>
+            <p>Full access for less than $1 a day</p>
+            <button>Sign Up</button>
+          </div>
+          <div>
+            <h2>Why Us</h2>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer & expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
