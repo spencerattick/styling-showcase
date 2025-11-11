0dcd5c8fd20c2b206bed34ca5cf6ab064ecb5cfb
Spencer Attick <spencer.attick@gmail.com>
2025-11-11 06:37:04 -0800

    add content

---
+++ src/app/cards/profile-card/page.tsx
@@ -0,0 +1,30 @@
+export default function ProfileCard() {
+  return (
+    <div>
+      <div>
+        <div></div>
+        <div>
+          <div>
+            <h1>Victor Crest</h1>
+            <h1>26</h1>
+          </div>
+          <p>London</p>
+          <div>
+            <div>
+                <h1>80K</h1>
+                <p>Followers</p>
+            </div>
+            <div>
+                <h1>803K</h1>
+                <p>Likes</p>
+            </div>
+            <div>
+                <h1>1.4K</h1>
+                <p>Photos</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
